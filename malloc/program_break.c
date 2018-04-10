6bbe5213e9ad9a7ec06ed52e261fa782c2cf5a27
tiagoft <tiagoft@gmail.com>
2018-04-10 13:24:30 -0300

    [master] Modificacao no program_break.c

--- malloc/program_break.c
+++ malloc/program_break.c
@@ -6,13 +6,19 @@ int main() {
   intptr_t  nbytes;
   void *end;
 
-
   end = sbrk(0);
-  printf("O final da minha memoria de programa esta em %lu\n", (long unsigned int)end);
+  printf("Comeco do heap: %p\n", end);
 
-  printf("Conteudo antes do fim da memoria: %d\n", *(int*)(end+4));
-  printf("Conteudo no fim da memoria: %d\n", *(int*)(end));
-  printf("Conteudo depois do fim da memoria: %d\n", *(int*)(end-4));
+
+  end = sbrk(10);
+  end = sbrk(-10);
+  end = sbrk(0);
+  printf("Fim do Heap: %p\n", end);
+
+  printf("Conteudo antes do fim do heap: %d\n", *(int*)(end-4));
+  printf("Conteudo no fim do heap: %d\n", *(int*)(end));
+  printf("Conteudo depois do fim do heap: %d\n", *(int*)(end+4));
 
   return 0;
 }
+
