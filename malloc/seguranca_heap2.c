055cd80fc33b8b9b081534b1b37b921dd8cd61f8
tiagoft <tiagoft@gmail.com>
2018-04-10 15:25:20 -0300

    [master] Seguranca usando malloc

--- malloc/seguranca_heap2.c
+++ malloc/seguranca_heap2.c
@@ -13,25 +13,38 @@ int main() {
   /* Codigo malicioso: parte 1 */
   void *start;
   char *c;
-  const unsigned long int size = 50000;
-
+  const unsigned long int size = 150;
   start = malloc(size);
   free(start);
+
 
   /* Inicializar memoria sensivel */
   memoria_sensivel = (char*) malloc (sizeof(char)*5);
   for (int i=0; i<5; i++) memoria_sensivel[i] = senha[i];
-  outra_memoria_sensivel = (char*) malloc (sizeof(char)*5);
+  outra_memoria_sensivel = (char*) malloc (sizeof(char)*10);
   for (int i=0; i<10; i++) outra_memoria_sensivel[i] = outra_senha[i];
 
+/*  printf("%p\n", memoria_sensivel);
+  printf("%p\n", outra_memoria_sensivel);
+*/
+  /*free(memoria_sensivel);
+  free(outra_memoria_sensivel);*/
 
+
+/*  printf("%p\n", start);
+*/
+
+  printf("Varrendo heap\n");
   /* Codigo malicioso: parte 2 */
-  end = sbrk(0);
-  for (c=(char*)0; c<(char*)size; c++) {
+  void *end = start + size;
+  for (c=(char*)start; c<(char*)end; c++) {
+    /*printf("%lu\n", (unsigned long int)((int)c-(int)start));*/
     if (*c != 0) {
-      printf("%05d %c\n", (long unsigned int)c, *(c+(char*)start));
+      printf("%p %c\n", c, *c);
     }
   }
 
   return 0;
 }
+
+
